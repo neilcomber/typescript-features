b91168fdaa2155dd06351c55647f1d84cdd4c26d
Neil Comber <neilcomber@gmail.com>
2023-10-08 07:44:58 +0100

    more practise

---
+++ annotations/arrays.ts
@@ -0,0 +1,19 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+  ['f150'],
+  ['corrola'],
+  ['camaro']
+];
+
+const car = carMakers[0];
+
+const myCar = carsByMake.pop();
+
+carMakers.push('66')
+
+console.log(carMakers.map((car: string): string => {
+  return car.concat('beef');
+}))
+
---
+++ interfaces.ts
@@ -0,0 +1,6 @@
+const oldCivic = {
+  name: 'civic', 
+  year: 2000, 
+  broken: true
+}
+
